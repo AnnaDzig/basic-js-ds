7b6ea01de7d2d3f3467a4f7c447068d37b915d4d
AnnaDzig <adzyhota@protonmail.com>
2025-11-10 21:41:14 +0100

    feat: implement Stack using internal array (push/pop/peek)

--- src/stack.js
+++ src/stack.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib/errors');
+//const { NotImplementedError } = require('../lib/errors');
 
 /**
  * Implement the Stack with a given interface via array.
@@ -13,19 +13,19 @@ const { NotImplementedError } = require('../lib/errors');
  *
  */
 class Stack {
-  push(/* value */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  push(value) {
+    if (!this._items) this._items = [];
+    this._items.push(value);
   }
 
   pop() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    if (!this._items || this._items.length === 0) return undefined;
+    return this._items.pop();
   }
 
   peek() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    if (!this._items || this._items.length === 0) return undefined;
+    return this._items[this._items.length - 1];
   }
 }
 
