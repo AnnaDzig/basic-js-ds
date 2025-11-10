a4630ff0b1bc9d66527e0ae31ce2a5bb804a34fe
AnnaDzig <adzyhota@protonmail.com>
2025-11-10 21:40:04 +0100

    feat: implement removeKFromList using linked-list traversal

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,5 +1,5 @@
-const { NotImplementedError } = require('../lib/errors');
-// const { ListNode } = require('../extensions/list-node.js');
+//const { NotImplementedError } = require('../lib/errors');
+ const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -21,9 +21,24 @@ const { NotImplementedError } = require('../lib/errors');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
+function removeKFromList( l, k ) {
   // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+  const dummy = new ListNode(0);
+  dummy.next = l;
+
+  let prev = dummy;
+  let cur = l;
+
+  while (cur) {
+    if (cur.value === k) {
+          prev.next = cur.next; 
+}else {
+      prev = cur;           
+    }
+    cur = cur.next;            
+  }
+
+  return dummy.next;
 }
 
 module.exports = {
